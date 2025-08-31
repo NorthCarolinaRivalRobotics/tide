793580b7caf981cdf47de833954ece2e1cdc0425
Ben Caunt <bdcaunt@gmail.com>
2025-08-30 23:52:46 -0300

    Add priority-based MuxNode component (#39)
    * Add priority-based MuxNode component
    
    * Update tide/components/mux_node.py
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * Update tide/components/mux_node.py
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * Update tide/components/mux_node.py
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- tide/components/__init__.py
+++ tide/components/__init__.py
@@ -3,5 +3,13 @@
 from .pid_node import PIDNode
 from .pose_estimator import PoseEstimatorNode, SE2Estimator, SE3Estimator
 from .webcam_node import WebcamNode
+from .mux_node import MuxNode
 
-__all__ = ["PIDNode", "PoseEstimatorNode", "SE2Estimator", "SE3Estimator", "WebcamNode"]
+__all__ = [
+    "PIDNode",
+    "PoseEstimatorNode",
+    "SE2Estimator",
+    "SE3Estimator",
+    "WebcamNode",
+    "MuxNode",
+]
